998ff2a3fdd85ae0524ad41604da37783b570099
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 08:57:07 +0000

    Adding tarfile member sanitization to extractall()

--- script.py
+++ script.py
@@ -95,7 +95,26 @@ def compute_duration(pkg_name, pkg_version):
 
         for d in glob.glob(DOWNLOAD_FOLDER + "/*.tar.gz"):
             with tarfile.open(d) as tar:
-                tar.extractall(path=APP_FOLDER)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=APP_FOLDER)
         # in case of executing for python-dateutil and setuptools packages
         # for d in glob.glob(DOWNLOAD_FOLDER + '/*.zip'):
         #   with zipfile.ZipFile(d, 'r') as zip_ref:
